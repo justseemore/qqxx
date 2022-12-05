8fe3d030f8b32f6eeb3bf83c4893d4fb1fde7f0b
zhaopengfei <hbyx.zpf@gmail.com>
2022-12-05 18:07:48 +0800

    fix

--- Crack/wyxw.js
+++ Crack/wyxw.js
@@ -3,9 +3,9 @@ Unlocks by Cuttlefish 公众号：墨鱼手记
 */
 var obj = JSON.parse($response.body);
 
-obj.vipInfo.expireDateStr = "2099/05/29";
-obj.vipInfo.remainderDays = 9999;
-obj.vipInfo.vipStatus = 1;
+obj.data.vipInfo.expireDateStr = "2099/05/29";
+obj.data.vipInfo.remainderDays = 9999;
+obj.data.vipInfo.vipStatus = 1;
 $done({body: JSON.stringify(obj)});
 
 //
